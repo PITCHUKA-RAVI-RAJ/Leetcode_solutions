31cf171fe220fb9265591f39da41fdf9dfe2d084
PITCHUKA RAVI RAJ <ravirajpitchuka@gmail.com>
2026-09-04 21:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (82.46%) - LeetSync

---
+++ 292-nim-game/nim-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n<=3:
+            return True
+        else:
+            if n-3>0:
+                if(n%4==0):
+                    return False
+                else:
+                    return True
+            else:
+                return False
